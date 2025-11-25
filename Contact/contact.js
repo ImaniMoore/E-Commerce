30f30245d54fb0a1022e6cddf2e7a4cbe872fc13
Imani Moore <imanimoore413@gmail.com>
2025-11-25 11:21:38 -0500

    first commit

---
+++ Contact/contact.js
@@ -0,0 +1,27 @@
+function validateForm(e) {
+  e.preventDefault();
+  let x = document.forms["myForm"]["Fname"].value;
+  let y = document.forms["myForm"]["Lname"].value;
+  let z = document.forms["myForm"]["email"].value;
+  let a = document.forms["myForm"]["phone"].value;
+  if (x == "") {
+    alert("First Name must be filled out");
+    return false;
+  } else if (y == "") {
+    alert("Last Name must be filled out");
+    return false;
+  } else if (z == "") {
+    alert("Email must be entered");
+    return false;
+  } else if (a == "") {
+    alert("Phone number must be entered");
+    return false;
+  } else {
+    alert("Submitted");
+    return true;
+  }
+}
+
+let contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", validateForm);
